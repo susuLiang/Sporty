ac3aae7480869f02b14ae9dfe0152cadccaf9e3d
Susu Liang <sophie790709@gmail.com>
2017-12-15 11:37:04 +0800

    Use picker chooses multiple and append into array

--- iSports/Lists/ListsController.swift
+++ iSports/Lists/ListsController.swift
@@ -13,83 +13,16 @@ class ListsController: UITableViewController {
     
     var results = [Preference]()
     
-    var isSearch: Bool = false
-//    @IBAction func searchAuthor() {
-//        searchResults = [Preference]()
-//        let ref = Database.database().reference().child("activities").queryOrdered(byChild: "type").queryEqual(toValue: "羽球")
-//        ref.observe(.value, with: { (snapshot: DataSnapshot) in
-//            guard let snapShotData = snapshot.value as? [String: AnyObject] else { return }
-//            for (activitiesId, activitiesData) in snapShotData {
-//                let id = activitiesId
-//                guard let type = activitiesData["type"] as? Sportstype else { return }
-//                guard let level = activitiesData["level"] as? Level else { return }
-//                guard let place = activitiesData["place"] as? String else { return }
-//                guard let time = activitiesData["time"] as? String else { return }
-//                let activities = Preference(type: type, level: level, place: place, time: time)
-//                self.searchResults.append(activities)
-//            }
-//            print(self.searchResults)
-//            self.tableView.reloadData()
-//        })
-//
-//
-//
-//
-//    }
-//    lazy var searchButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
-//        button.setTitle("Search", for: .normal)
-////        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.frame = CGRect(x: 300, y: 0, width: 50, height: 50)
-////        button.setTitleColor(UIColor.white, for: .normal)
-//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-//
-//        button.addTarget(self, action: #selector(searchAuthor(type:)), for: .touchUpInside)
-//        return button
-//    }()
-    
-//    lazy var searchTextField: UITextField = {
-//        let tf = UITextField()
-//        tf.backgroundColor = UIColor.white
-//        tf.autocapitalizationType = .none
-//        tf.placeholder = "🔍 Author Name"
-//        tf.textAlignment = .center
-//        if let tfWitdth = self.navigationController?.navigationBar.frame.width,
-//            let tfHeight = self.navigationController?.navigationBar.frame.height {
-//            tf.frame = CGRect(x: 0, y: 0, width: tfWitdth * 0.6, height: tfHeight * 0.8)
-//        } else {
-//            tf.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
-//        }
-//        tf.delegate = self
-//        return tf
-//    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableCell()
-        navigationItem.title = "Title"
-        print(navigationItem)
-
-        
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "icon-home"), for: .normal)
-
-        button.tintColor = UIColor.black
-        button.addTarget(self, action: #selector(searchAuthor), for: .touchUpInside)
-        let barButton = UIBarButtonItem(customView: button)
-        self.navigationItem.rightBarButtonItem = barButton
+        setNavigation()
         
         tableView.delegate = self
         tableView.dataSource = self
         
         fetch()
-        
-        
-        
-        
-
-       
+  
     }
 
     override func didReceiveMemoryWarning() {
@@ -97,15 +30,8 @@ class ListsController: UITableViewController {
     }
     
     func setupTableCell() {
-        let nib = UINib(
-            nibName: "ListsCell",
-            bundle: nil
-        )
-        
-        tableView.register(
-            nib,
-            forCellReuseIdentifier: "cell"
-        )
+        let nib = UINib(nibName: "ListsCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "cell")
     }
 
 
@@ -116,13 +42,13 @@ class ListsController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(results.count)
         return results.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ListsCell 
-            print(self.results)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ListsCell
             cell.titleLabel.text = results[indexPath.row].id
             cell.timeLabel.text = results[indexPath.row].time
             cell.levelLabel.text = results[indexPath.row].level.rawValue
@@ -139,19 +65,18 @@ class ListsController: UITableViewController {
     
     @objc func searchAuthor() {
         results = [Preference]()
-        let ref = Database.database().reference().child("activities").queryOrdered(byChild: "type").queryEqual(toValue: "羽球").queryOrdered(byChild: "time").queryEqual(toValue: "星期一")
+        let ref = Database.database().reference().child("activities").queryOrdered(byChild: "type").queryEqual(toValue: "badminton")
         ref.observe(.value, with: { (snapshot: DataSnapshot) in
             guard let snapShotData = snapshot.value as? [String: AnyObject] else { return }
             for (activitiesId, activitiesData) in snapShotData {
                 let id = activitiesId
-                guard let type = activitiesData["type"] as? Sportstype else { return }
-                guard let level = activitiesData["level"] as? Level else { return }
+                guard let type = activitiesData["type"] as? String else { return }
+                guard let level = activitiesData["level"] as? String else { return }
                 guard let place = activitiesData["place"] as? String else { return }
                 guard let time = activitiesData["time"] as? String else { return }
-                let activities = Preference(id: id,type: type, level: level, place: place, time: time)
+                let activities = Preference(id: id, type: Sportstype(rawValue: type)!, level: Level(rawValue: level)!, place: place, time: time)
                 self.results.append(activities)
             }
-            print(self.results)
             self.tableView.reloadData()
         })
     }
@@ -203,6 +128,24 @@ extension ListsController {
             }
         }
     }
+}
+
+extension ListsController {
+    
+    func setNavigation() {
+        navigationItem.title = "Title"
+        
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "icon-home"), for: .normal)
+        
+        button.tintColor = UIColor.black
+        button.addTarget(self, action: #selector(searchAuthor), for: .touchUpInside)
+        let barButton = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = barButton
+        
+    }
+    
+    
 }
 
         
--- iSports/MainViewController.swift
+++ iSports/MainViewController.swift
@@ -10,45 +10,133 @@ import UIKit
 import Firebase
 
 class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 1
-    }
+    var level: [Level] = [.A, .B, .C, .D]
+    var type: [Sportstype] = [  .basketball,
+                                .volleyball,
+                                .baseball,
+                                .football,
+                                .badminton,
+                                .tennis,
+                                .bowling]
+    var city: [String] = ["信義區", "大安區", "松山區", "中正區", "中山區"]
+    var time = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    
+    var choosedtype: String?
+    var choosedlevel: String?
+    var choosedcity: String?
+    var choosedtime: String?
+
     
     
     var searchResults = [Preference]()
-     @IBOutlet weak var searText: UITextField!
+    @IBOutlet weak var typeText: UITextField!
+    @IBOutlet weak var levelText: UITextField!
+    @IBOutlet weak var placeText: UITextField!
+    @IBOutlet weak var timeText: UITextField!
+    
+    var typePicker = UIPickerView()
+//    var levelPicker = UIPickerView()
+//    var placePicker = UIPickerView()
+//    var timePicker = UIPickerView()
+//
 
     @IBOutlet weak var numLabel: UILabel!
-    
+   
     @IBAction func search(_ sender: Any) {
         searchResults = [Preference]()
-        let ref = Database.database().reference().child("activities").queryOrdered(byChild: "type").queryEqual(toValue: "羽球")
+        let ref = Database.database().reference().child("activities").queryOrdered(byChild: "type").queryEqual(toValue: choosedtype)
         ref.observe(.value, with: { (snapshot: DataSnapshot) in
             print(snapshot.value)
             guard let snapShotData = snapshot.value as? [String: AnyObject] else { return }
             for (activitiesId, activitiesData) in snapShotData {
                 let id = activitiesId
-                guard let type = activitiesData["type"] as? Sportstype else { return }
-                guard let level = activitiesData["level"] as? Level else { return }
+                guard let type = activitiesData["type"] as? String else { return }
+                guard let level = activitiesData["level"] as? String else { return }
                 guard let place = activitiesData["place"] as? String else { return }
                 guard let time = activitiesData["time"] as? String else { return }
-                let activities = Preference(id: id, type: type, level: level, place: place, time: time)
+                if time == self.choosedtime && level == self.choosedlevel && place == self.choosedcity{
+                    let activities = Preference(id: id, type: Sportstype(rawValue: type)!, level: Level(rawValue: level)!, place: place, time: time)
                 self.searchResults.append(activities)
+                }
             }
             print(self.searchResults)
-            self.viewDidLoad()
-//            self.tableView.reloadData()
+
         })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         numLabel.text = String(searchResults.count)
-        // Do any additional setup after loading the view.
+        typeText.inputView = typePicker
+        typePicker.delegate = self
+        typePicker.dataSource = self
+//        timeText.inputView = timePicker
+//        levelText.inputView = levelPicker
+//        placeText.inputView = placePicker
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    @objc func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        switch component {
+        case 1:
+            return type.count
+        case 2:
+            return level.count
+        case 3:
+            return time.count
+        case 4:
+            return city.count
+        default:
+            return 0
+        }
+
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 1:
+            return type[row].rawValue
+        case 2:
+            return level[row].rawValue
+        case 3:
+            return time[row]
+        case 4:
+            return city[row]
+        default:
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 1:
+            typeText.text = type[row].rawValue
+            choosedtype = type[row].rawValue
+        case 2:
+            levelText.text = level[row].rawValue
+            choosedlevel = level[row].rawValue
+        case 3:
+            timeText.text = time[row]
+            choosedtime = time[row]
+        case 4:
+            placeText.text = city[row]
+            choosedcity = city[row]
+        default:
+            return 
+        }
     }
 
     override func didReceiveMemoryWarning() {
